5d37a6411bcdb3588e163bd64eb6aa88b0cecb6a
Pratham Patel <pratham.code10@gmail.com>
2026-01-19 15:01:11 -0600

    Added form to create habits

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+export default function App() {
+	function handleCreateClick() {}
 
-export default App;
+	return (
+		<>
+			<h1>Welcome to Habit Tracker</h1>
+			<CreateForm />
+		</>
+	);
+}
+
+function CreateForm() {
+	return (
+		<div className="create-form">
+			<h3>Create a new Habit!</h3>
+			<form>
+				<label for="createHabitName">Habit Name</label>
+				<br />
+				<input
+					type="text"
+					id="createHabitName"
+					name="createHabitName"
+				></input>
+				<br></br>
+				<input type="submit" value={"Create"}></input>
+			</form>
+		</div>
+	);
+}
