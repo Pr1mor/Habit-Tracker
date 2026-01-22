773c2a80e6e899f24365ee4a26e7d0bf6d425e4e
Pratham Patel <pratham.code10@gmail.com>
2026-01-22 15:21:46 -0600

    updated the deploy.yml and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://github.com/Pr1mor/Habit-Tracker/", // so that I can deloy my website on github
+	base: "/Habit-Tracker/", // so that I can deloy my website on github
 });
